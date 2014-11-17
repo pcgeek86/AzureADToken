9ca247d321df1b6758e91799426c7136e0840373
Trevor Sullivan <trevor@opsgility.com>
2014-11-17 14:53:47 -0800

    Initial commit

---
+++ AzureADToken.psm1
@@ -0,0 +1,22 @@
+﻿function Get-AzureADToken {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [string] $TenantId
+      , [Parameter(Mandatory = $true)]
+        [string] $ClientId
+      , [Parameter(Mandatory = $true)]
+        [System.Uri] $RedirectUri
+    )
+
+    Add-Type -Path ('{0}\Microsoft.IdentityModel.Clients.ActiveDirectory.2.12.111071459\lib\net45\Microsoft.IdentityModel.Clients.ActiveDirectory.dll' -f $PSScriptRoot);
+    Write-Verbose -Message 'Finished loading Microsoft Azure Active Directory Authentication Library';
+
+    [Microsoft.IdentityModel.Clients.ActiveDirectory.AuthenticationResult]$Result = $null;
+    $Context = New-Object -TypeName Microsoft.IdentityModel.Clients.ActiveDirectory.AuthenticationContext -ArgumentList ('https://login.windows.net/{0}' -f $TenantId);
+
+    $Endpoint = 'https://management.core.windows.net/';
+    $Context.AcquireToken($Endpoint, $ClientId, $RedirectUri);
+}
+
+Export-ModuleMember -Function Get-AzureADToken;
---
+++ Test-Get-AzureADToken.ps1
@@ -0,0 +1,11 @@
+﻿$TenantId = '8ae1b885-4ca2-43b8-ae5e-3f66ee470766';
+$ClientId = '84f4d798-01ef-423b-833d-7d385b94cbf9';
+$RedirectUri = 'http://localhost/powershell';
+
+# Authenticate to Microsoft Azure
+$Token = Get-AzureADToken -TenantId $TenantId -ClientId $ClientId -RedirectUri $RedirectUri;
+
+# Get the Access Token (aka "bearer")
+$AccessToken = $Token.AccessToken;
+
+$AccessToken | clip;
